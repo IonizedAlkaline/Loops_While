481adf28eb11fba3fdbf14bd0c12028cfd4505a1
IonizedAlkaline <rudranshm007@gmail.com>
2025-03-22 19:10:41 -0700

    Add files via upload

---
+++ Armstrong.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a number: "))
+temp = n
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + digit**3
+    temp = temp // 10
+
+if sum == n:
+    print("armstrong")
+else:
+    print("Not Armstrong")
